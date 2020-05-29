80a4dfc511d3889b74e009bf7113dad776e30dea
Cory Milsap <crmilsap@crimson.ua.edu>
2020-05-29 05:03:59 -0500

    Baseplate for website

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import navbar from './Components/navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <video> 
+
+       </video>
+
+       <img>
+       
+       </img>
+
+       <svg>
+         
+       </svg>
+      </header>
+      <navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/navbar.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+
+function Navbar() {
+
+    return (
+        <nav className = 'nav'>
+          <h1>
+            About Me
+          </h1>
+          <h1>
+            Experience
+          </h1>
+          <h1>
+            Projects
+          </h1>
+          <h1>
+            Skills
+          </h1>
+        </nav>
+    );
+}  
+
+export default Navbar
